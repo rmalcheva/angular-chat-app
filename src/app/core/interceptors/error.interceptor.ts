8533e8f22ee60c9d9c2f2b4bb95282ec8092a878
r-malcheva <radina.malcheva22@gmail.com>
2025-03-13 19:10:59 +0200

    feat: add interceptor for handling error messages globally

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -4,25 +4,27 @@ import { Router } from 'express';
 import { catchError, of, throwError } from 'rxjs';
 import { RoutesPaths } from '../../shared/models/routes';
 import { AuthService } from '../services/auth.service';
+// import { ToastrService } from 'ngx-toastr';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       const authService = inject(AuthService);
+      // const toastr = inject(ToastrService);
       if (error) {
         switch (error.status) {
           case 401:
             authService.clearSession();
-            console.log(error.statusText);
+            // toastr.error(error.statusText);
             break;
           case 403:
-            console.log(error.statusText);
+            // toastr.error(error.statusText);
             break;
           case 404:
-            console.log(error.statusText);
+            // toastr.error(error.statusText);
             break;
           case 500:
-            console.log(error.statusText);
+            // toastr.error(error.statusText);
             break;
           default:
             console.log('Something went wrong.Please try again later.');
